7522a385ecaace50f2a832d56d2135b643b05d18
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-08-01 17:17:04 -0400

    renaming file, adding URL suffixes

---
+++ get_listings.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import lp_links
+
+
+
+driver = webdriver.Chrome()
+timeout = 5
+linksPresent = False
+reqSubStr = 'https://www.listingsproject.com/listings/'
+links = []
+
+
+# Older selenium syntax
+# driver.find_element_by_xpath("//input[@type='submit' and @value='something']").click()
+# newer selenium synntax
+# browser.find_element(By.XPATH, '//button[text()="Outliers"]')
+
+def get_listings():
+    for suffix in lp_links.suffixes:
+        driver.get(lp_links.base_url + suffix)
+        try:
+            driver.find_element(By.XPATH, '//button[text()="Ok, got it"]').click()
+            driver.find_element(By.XPATH, '//input[@type="submit" and @value="I accept. Take me to the listings."]').click()
+        except:
+            pass
+
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//a[@href]'))
+            WebDriverWait(driver, timeout).until(element_present)
+            linksPresent = True
+        except TimeoutException:
+            print('Timed out waiting for page to load')
+
+        if linksPresent:
+            elems = driver.find_elements(By.XPATH, '//a[@href]')
+            #print(type(elems))
+            #print(len(elems))
+            for elem in elems:
+                elemLink = elem.get_attribute("href")
+                if reqSubStr not in elemLink:
+                    place = elems.index(elem)
+                    elems.pop(place)
+                    continue
+                # type: class 'selenium.webdriver.remote.webelement.WebElement'
+                links.append(elemLink) # class 'str'
+    uniqueLinks = [*set(links)]
+    #print(len(uniqueLinks))
+    driver.quit()
+    return uniqueLinks
+
--- lp_links.py
+++ lp_links.py
@@ -3,10 +3,12 @@ from get_last_wed import get_last_wed as wed
 last_wed = wed()
 base_url = "https://www.listingsproject.com/newsletter/" + str(last_wed) + "/new-york-city/"
 
-room_rent_url = "room_rent"
-room_sub_url = "room_sublet"
-apt_rent_url = "apt_rent"
-apt_sub_url = "apt_sublet"
+suffixes = ["room_rent","room_sublet","apt_rent","apt_sublet"]
+
+# room_rent_url = "room_rent"
+# room_sub_url = "room_sublet"
+# apt_rent_url = "apt_rent"
+# apt_sub_url = "apt_sublet"
 
 
 
